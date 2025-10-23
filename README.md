3f041690183f8a6b0317264ca377456e0a2d41ab
Brian Kulyk <brian@kulyk.ca>
2025-10-23 08:51:20 -0700

    adding ability to laucnh with blastem emulator and istructions on how to compile blastem on apple hardware

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # Game
 
 This is going to be a game. Likely developed in C with SGDK/Mars. I'm still figuring it out
+
+## Building
+
+```sh
+# build with the mars toolchain, launch with emulator
+task dev
+```
+
+## Rambling/Planning
+
+- Robo Aleste main sprite was 32x45 (without the 2 options)
+
+## Emulators
+
+I found 2 emulators to work for Mac [genesis plus] and [blastem].
+
+### Genesis Plus
+
+Seem to work well can open roms from cli like: `open -s "genesis plus" rom.bin`.
+
+Genesis Plus doesn't seem to have much in the way of debugging.
+
+### Blastem
+
+I had to compile it myself to get it to work with my M4 laptop.
+
+```sh
+# get dependency, may require others that were already installed for me.
+brew install glew
+# Add missing CPU core files using the project's cpu_dsl.py script
+python3 cpu_dsl.py -d call m68k.cpu > m68k.c
+python3 cpu_dsl.py -d call z80.cpu > z80.c
+python3 cpu_dsl.py -d call upd78k2.cpu > upd78k2.c
+# compile
+make
+```
+
+[blastem]: https://www.retrodev.com/blastem/
+[genesis plus]: https://segaretro.org/Genesis_Plus
--- src/main.c
+++ src/main.c
@@ -7,30 +7,29 @@ typedef enum { SPLASH, TITLE, GAME, GAME_OVER } AppState;
 AppState appState = SPLASH;
 
 void game() {
-    VDP_drawText("Do game stuff.", 12, 20);
+  VDP_drawText("Do game stuff.", 12, 20);
+  SYS_enableInts();
 
-    SYS_enableInts();
-
-    // game loop
-    while (true) {
-        SYS_doVBlankProcess();
-    }
+  // game loop
+  while (true) {
+    SYS_doVBlankProcess();
+  }
 }
 
 int main(bool hardReset) {
-    SYS_disableInts();
-    VDP_setScreenWidth320();
-    PAL_setColors(0, (u16*) palette_black, 64, DMA);
+  SYS_disableInts();
+  VDP_setScreenWidth320();
+  PAL_setColors(0, (u16 *)palette_black, 64, DMA);
 
-    while (true) {
-        if (appState == SPLASH) {
-            splash();
-            appState = GAME;
-        } else if (appState == GAME) {
-            game();
-        } else {
-            // nothing yet
-        }
+  while (true) {
+    if (appState == SPLASH) {
+      splash();
+      appState = GAME;
+    } else if (appState == GAME) {
+      game();
+    } else {
+      // nothing yet
     }
-    return (0);
+  }
+  return (0);
 }
